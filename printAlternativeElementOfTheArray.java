41b9605c24541192251804a0002c01a67da8bf02
Vibishraj <128944659+vibishraj001@users.noreply.github.com>
2024-02-25 08:45:11 +0530

    first commit
    You are given an array A of size N. You need to print elements of A in alternate order (starting from index 0).
      Example  :
    
    Input:
    N = 4
    A[] = {1, 2, 3, 4}
    Output:
    1 3

---
+++ printAlternativeElementOfTheArray.java
@@ -0,0 +1,10 @@
+class solution
+{
+    public static void print(int arr[], int n)
+    {
+      for (int i=0; i<n;i=i+2)
+      {
+          System.out.print(arr[i]+ " ");
+      }// your code here
+    }
+}
